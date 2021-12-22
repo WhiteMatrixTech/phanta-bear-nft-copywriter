d6a8f40319c2060110f493d0de5fd97db8b2f1d8
张瑞 <zrnju@qq.com>
2021-12-22 17:30:02 +0800

    add home introduction

--- lib/buy.ts
+++ lib/buy.ts
@@ -1,6 +1,6 @@
 export default {
     zh: {
-        buy_introduction: 'Phanta Bear (PHB) 是由 PHANTACi 和 EZEK 联合推出的 10,000 个随机生成的数字收藏品。 每只Phanta Bear都是独特的，都会解锁不同的访问和特权级别。'
+        buy_introduction: 'Phanta Bear (PHB) 是由 PHANTACi 和 EZEK 联合推出的 10,000 个随机生成的数字收藏品。每只Phanta Bear都是独特的，都会解锁不同的访问和特权级别。'
     },
     en: {
         buy_introduction: 'Phanta Bear (PHB) is 10,000 randomly generated digital collectible jointly launched by Phantaci and EZEK.  Each Phanta Bear has unique traits, and each PHB unlocks varying and unique levels of access and Perks. '
