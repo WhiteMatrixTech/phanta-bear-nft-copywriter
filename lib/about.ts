1a4969d9fbec97ee6a4a11d671e836c6937796c9
feibowei <1906524955@qq.com>
2021-12-24 10:10:46 +0800

    feat: update about to add two links

--- lib/about.ts
+++ lib/about.ts
@@ -4,14 +4,18 @@ export default {
         p2: 'PHANTACi 是由拥有源源不绝创意灵感的 Jay (周杰伦) 以及对于流行产业有着独到见解的 Ric 共同打造的服饰品牌，于2006年创立。将源自于奇幻壮丽的设计与想法融入日常生活当中。',
         p3: '现在我们想将顶级时尚愿景与我们新的区块链时代相结合，建立 Phanta Bear NFT。 Phanta Bear(PHB)拥有两只明亮的眼睛，内心深处注入了中国流行天王的艺术天赋。 10000个经典元素的随机艺术组合和丰富的线上线下权益，让拥有者尽享元宇宙生活之道。 ',
         p4: '如何在谷歌浏览器上安装和使用MetaMask钱包?',
+        link4: 'https://d1kjtx52rxv2sn.cloudfront.net/website/pdf/%E5%A6%82%E4%BD%95%E5%9C%A8%E8%B0%B7%E6%AD%8C%E6%B5%8F%E8%A7%88%E5%99%A8%E4%B8%8A%E5%AE%89%E8%A3%85%E5%92%8C%E4%BD%BF%E7%94%A8MetaMask%E9%92%B1%E5%8C%85.pdf',
         p5: '如何购买以太币并转移到Web3钱包（含Coinbase）?',
+        link5: 'https://d1kjtx52rxv2sn.cloudfront.net/website/pdf/%E5%A6%82%E4%BD%95%E8%B4%AD%E4%B9%B0%E4%BB%A5%E5%A4%AA%E5%B8%81%E5%B9%B6%E8%BD%AC%E7%A7%BB%E5%88%B0Web3%E9%92%B1%E5%8C%85%EF%BC%88%E5%90%ABCoinbase%EF%BC%89.pdf'
     },
     en: {
         p1: 'When we starting to shoot the movie ‘The Roof’ that self directed and performed by Jay in 2013, there’s an idea that has been plotted in our mind. Is there a way for our viewers that not only just watch our movie, but truely, completely immersed in the scene?',
         p2: 'Since The fasion icon Jay Chou and his crew founded PHANTACi in 2006. The original concept of PHANTACi came from the nastiness the word Phantom conveys, and the great magnificence the idea of Fantasy can possibly reach.',
         p3: 'Now we want to combine the top fashion vision with our new blockchain era icon to build up Phanta Bear NFT. Phanta Bear PHB with two eyes of star, injected deeply inside with the Mandopop King’s talent on Art. 10000 Random artistic combination of Phantaci Classic elements with great online & offline rights to let owners enjoy the way of Metaverse Life.',
         p4: 'How to Install and Use Metamask on Google Chrome?',
+        link4: 'https://d1kjtx52rxv2sn.cloudfront.net/website/pdf/How+to+Install+and+Use+Metamask+on+Google+Chrome.pdf',
         p5: 'How to buy ETH and transfer it to a web3 wallet?',
-   
+        link5: 'https://d1kjtx52rxv2sn.cloudfront.net/website/pdf/How+to+buy+ETH+and+transfer+it+to+a+web3+wallet.pdf'
+
     }
-  };
+};
