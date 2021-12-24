291bb3fcb6ad14c295d8581a61d4a52b180e3fc2
张瑞 <zrnju@qq.com>
2021-12-24 10:58:38 +0800

    update

--- lib/buy.ts
+++ lib/buy.ts
@@ -1,8 +1,8 @@
 export default {
     zh: {
-        buy_introduction: 'Phanta Bear (PHB) 是由 PHANTACi 和 EZEK 联合推出的 10,000 个随机生成的数字收藏品。每只Phanta Bear都是独特的，都会解锁不同的访问和特权级别。'
+        buy_introduction: 'Phanta Bear (PHB) 是由 PHANTACi 和 EZEK 联合推出的 10,000 个随机生成的数字收藏品，可兼作 Fantasy Club 的会员卡。。每只Phanta Bear都是独特的，都会解锁不同的访问和特权级别。PHB项目由 PHANTACi 和 Ezek 联合发起。'
     },
     en: {
-        buy_introduction: 'Phanta Bear (PHB) is 10,000 randomly generated digital collectible jointly launched by Phantaci and EZEK.  Each Phanta Bear has unique traits, and each PHB unlocks varying and unique levels of access and Perks. '
+        buy_introduction: 'Phanta Bear (PHB) is a collection of 10,000 algorithmically generated digital collectibles that double as memebership cards for the Fantasy Club. Each Phanta Bear has a unique set of traits and unlocks varying, unique levels of access and perks for its owner. The PHB project was jointly launched by PHANTACi and Ezek.'
     }
   };
