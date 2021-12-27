d3d0589d8c045315ed58f7f289cc7b8c919992f6
张瑞 <zrnju@qq.com>
2021-12-27 08:21:30 +0800

    some changes

--- lib/signup.ts
+++ lib/signup.ts
@@ -1,6 +1,6 @@
 export default {
     zh: {
-        signup: '登录',
+        signup: '绑定邮箱',
         email_address: '邮箱地址',
         email_address_error: '请输入有效的邮箱地址',
         send_verification_code: '发送验证码',
@@ -9,11 +9,11 @@ export default {
         send_verification_code_mobile: '发送',
         verification_code: '验证码',
         verification_code_error: '验证码错误',
-        sign_me_up: '现在登录!',
+        sign_me_up: '现在绑定!',
         email_has_been_bound: '邮箱已经被绑定过'
     },
     en: {
-        signup: 'SIGNUP',
+        signup: 'ADD YOUR EMAIL',
         email_address: 'Email Address',
         email_address_error: 'Please enter a valid email address.',
         send_verification_code: 'Send Verification Code',
@@ -22,7 +22,7 @@ export default {
         send_verification_code_mobile: 'Send',
         verification_code: 'Verification Code',
         verification_code_error: 'The verification code is incorrect.',
-        sign_me_up: 'Sign me up!',
+        sign_me_up: 'Add Now!',
         email_has_been_bound: 'Email has already been bound.'
     }
   };
