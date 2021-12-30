73a6d69e301eaf69d2b7bf58af3bf8aad43b4112
张瑞 <zrnju@qq.com>
2021-12-30 18:02:31 +0800

    Update buy.ts

--- lib/buy.ts
+++ lib/buy.ts
@@ -3,10 +3,28 @@ export default {
         price: '价格',
         amount: '数量',
         buy_introduction: 'Phanta Bear 是10,000 个随机生成的数字收藏品，同时可兼作 Ezek Club 的会员卡。每只Phanta Bear都是独特的，都会解锁不同的等级和会员权益。Phanta Bear项目由 PHANTACi 和 Ezek 联合发起。'
+        buy_button: '购买',
+        buy_via_whitelist: '白名单购买',
+        buy_via_whitelist_info1: '数量: {{count}} Phanta Bear(s)',
+        buy_via_whitelist_info2: '时间: 上午11:00 ~ 下午1:00 2022年1月1日 UTC+8',
+        view_in_opensea: '去OPENSEA查看',
+        buy_success_title: '恭喜!',
+        buy_success_content1: '您已成功拥有 {count} 只 Phanta Bear(s)!',
+        buy_success_content2: '稍后您可以在个人主页查看。'
+
     },
     en: {
         price: ' Price',
         amount: 'Amount',
         buy_introduction: 'Phanta Bear is a collection of 10,000 algorithmically generated digital collectibles that double as memebership cards for the Ezek Club. Each Phanta Bear has a unique set of traits and unlocks varying, unique levels of access and perks for its owner. Phanta Bear project was jointly launched by PHANTACi and Ezek.'
+        buy_button: 'BUY NOW',
+        buy_via_whitelist: 'BUY VIA WHITELIST',
+        buy_via_whitelist_info1: 'Amount: {{count}} Phanta Bear(s)',
+        buy_via_whitelist_info2: 'Time: 11:00 AM ~ 1:00 PM Jan. 1st 2022 UTC+8',
+        view_in_opensea: 'VIEW IN OPENSEA',
+        buy_success_title: 'Congratulations!',
+        buy_success_content1: 'You’ve successfully owned {count} Phanta Bear(s)!',
+        buy_success_content2: 'You can view them in profile page later.'
+
     }
   };
