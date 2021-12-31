17ae48abec9344eb17b32e89f7efb52cf99d8c71
caoxm <386707656@qq.com>
2021-12-31 11:14:02 +0800

    remove comming soo

--- lib/all-products.ts
+++ lib/all-products.ts
@@ -1,10 +1,10 @@
 export default {
     zh: {
-        coming_soon: '即将开始！',
-        empty_list: '暂无数据！'
+        coming_soon: '暂无数据',
+        empty_list: '暂无数据'
     },
     en: {
-        coming_soon: ' COMING SOON!',
+        coming_soon: 'NO DATA HERE',
         empty_list: 'NO DATA HERE'
     }
   };
