a2f223eb2d2e8fbdc02091ef356fdd109ec68a8c
张瑞 <zrnju@qq.com>
2021-12-31 21:05:05 +0800

    Update owned.ts

--- lib/owned.ts
+++ lib/owned.ts
@@ -4,7 +4,7 @@ export default {
         you_have_bear: '您拥有 {{count}} 个PHANTA BEARS',
     },
     en: {
-        owned: 'OWNED',
+        owned: 'My Bear(s)',
         you_have_bear: 'YOU OWNED {{count}} PHANTA BEARS',
     }
   };
