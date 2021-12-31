f8800da7ba9682c11d246399b22dccd1b7602353
caoxm <386707656@qq.com>
2021-12-31 11:02:12 +0800

    fix ,,,,,,

--- lib/buy.ts
+++ lib/buy.ts
@@ -2,7 +2,7 @@ export default {
     zh: {
         price: '价格',
         amount: '数量',
-        buy_introduction: 'Phanta Bear 是10,000 个随机生成的数字收藏品，同时可兼作 Ezek Club 的会员卡。每只Phanta Bear都是独特的，都会解锁不同的等级和会员权益。Phanta Bear项目由 PHANTACi 和 Ezek 联合发起。'
+        buy_introduction: 'Phanta Bear 是10,000 个随机生成的数字收藏品，同时可兼作 Ezek Club 的会员卡。每只Phanta Bear都是独特的，都会解锁不同的等级和会员权益。Phanta Bear项目由 PHANTACi 和 Ezek 联合发起。',
         buy_button: '购买',
         buy_via_whitelist: '白名单购买',
         buy_via_whitelist_info1: '数量: {{count}} Phanta Bear(s)',
@@ -16,7 +16,6 @@ export default {
     en: {
         price: ' Price',
         amount: 'Amount',
-        buy_introduction: 'Phanta Bear is a collection of 10,000 algorithmically generated digital collectibles that double as memebership cards for the Ezek Club. Each Phanta Bear has a unique set of traits and unlocks varying, unique levels of access and perks for its owner. Phanta Bear project was jointly launched by PHANTACi and Ezek.'
         buy_introduction: 'Phanta Bear is a collection of 10,000 algorithmically generated digital collectibles that double as memebership cards for the Ezek Club. Each Phanta Bear has a unique set of traits and unlocks varying, unique levels of access and perks for its owner. Phanta Bear project was jointly launched by PHANTACi and Ezek.',
         buy_button: 'BUY NOW',
         buy_via_whitelist: 'BUY VIA WHITELIST',
