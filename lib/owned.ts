b9a11577c7b8dd2f9623b9a391d359eee839a499
张瑞 <zrnju@qq.com>
2022-01-28 09:45:11 +0800

    Update owned.ts

--- lib/owned.ts
+++ lib/owned.ts
@@ -4,7 +4,8 @@ export default {
         you_have_bear: '您拥有 {{count}} 个PHANTA BEARS',
         claim_info: '恭喜您收到了我们的特别NFT空投，请点击下面的按钮来获得您的NFT(s)。感谢您的支持！',
         claim_button: '现在就CLAIM!',
-        claim_button_info: '一次最多只能CLAIM50个',
+        claim_button_info: '一次最多只能CLAIM 50 个',
+        claim_succese: 'CLAIM 成功！'
 
     },
     en: {
@@ -12,6 +13,8 @@ export default {
         you_have_bear: 'YOU OWNED {{count}} PHANTA BEARS',
         claim_info: 'You’ve received our special NFT airdrop, please click the button below to claim your NFT(s). Thanks for your support!',
         claim_button: 'CLAIM NOW!',
-        claim_button_info: 'You can only claim 50 per time.'
+        claim_button_info: 'You can only claim 50 per time.',
+        claim_succese: 'CLAIM successfully！'
+
     }
   };
